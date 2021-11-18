5c55abbaa9095a1603659925259667d5df1e58e5
riri0602 <73737992+riri0602@users.noreply.github.com>
2021-11-18 14:06:52 +0900

    Add files via upload

---
+++ 중간고사/한규리_JsonTest.js
@@ -0,0 +1,12 @@
+const data = {
+    code: 42,
+    items: [{
+        id: 1,
+        name: 'foo'
+    }, {
+        id: 2,
+        name: 'bar'
+    }]
+};
+var n = 18681032;
+console.log('아이템 아이디는 ' + data.items[0].id + '이고, 아이템 이름은 ' + data.items[0].name + '입니다')
